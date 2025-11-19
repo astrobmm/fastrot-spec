69e379e8c684dcbe5798351fa4fb6d41e3a9afcc
Benjamín Montesinos <bmm@cab.inta-csic.es>
2025-11-19 14:26:50 +0100

    Add files via upload

--- 00_codes/fastrot-highres-spec.sh
+++ 00_codes/fastrot-highres-spec.sh
@@ -4,27 +4,45 @@ echo "     *****************************************"
 echo "     *        PROGRAM  star3d running        *"
 echo "     *****************************************"
 ./star3d.exe
+./grid10.exe
+
+mv in*.params    ../11_results/.
+mv mer*.a        ../22_for_plotting/.
+mv par*.a        ../22_for_plotting/.
+mv colat-r-t.a   ../22_for_plotting/.
+
 echo "   "
-echo "     *****************************************"
-echo "     *      PROGRAM  highrespec3d running    *"
-echo "     *****************************************"
+echo "     ******************************************"
+echo "     *      PROGRAM  highrespec3d running     *"
+echo "     ******************************************"
 
 ./highrespec3d.exe
-echo "   "
-echo "     *****************************************"
-echo "     *       PROGRAM  normspec running       *"
-echo "     *****************************************"
-
+./mergefiles.exe
 ./normspec.exe
-mv *.params ../11_results/.
-mv *.a      ../11_results/.
-mv *.n1     ../11_results/.
+
+cp in*.a      ../11_results/.
+cp in*.n1     ../11_results/.
+mv in*.a      ../22_for_plotting/.
+mv in*.n1     ../22_for_plotting/.
+mv 4plots.a   ../22_for_plotting/.
+
+rm -f *.a
+
 echo "   "
-echo "     **********************************************"
-echo "     *       Synthetic spectra (in physical       *"
-echo "     *       units, and normalized) and stellar   *"
-echo "     *       parameters are in folder 11_results   *"
-echo "     **********************************************"
+echo "     **************************************************************"
+echo "     *                                                            *"  
+echo "     *     Synthetic spectrum in physical units (extension .a)    *"
+echo "     *     and normalized (extension .n1), and relevant stellar   *"
+echo "     *     parameters are in folder:                              *"
+echo "     *                                                            *"
+echo "     *     11_results/                                            *"
+echo "     *                                                            *"
+echo "     *     Additional output files for plotting, including the    *"
+echo "     *     the spectra, are in folder:                            *"
+echo "     *                                                            *"
+echo "     *     22_for_plotting/                                       *"
+echo "     *                                                            *"
+echo "     **************************************************************"
 echo "   "
 
 exit
--- 00_codes/fastrot-lowres-spec.sh
+++ 00_codes/fastrot-lowres-spec.sh
@@ -4,22 +4,44 @@ echo "     *****************************************"
 echo "     *        PROGRAM  star3d running        *"
 echo "     *****************************************"
 ./star3d.exe
+./grid10.exe
+
+mv in*.params    ../11_results/.
+mv mer*.a        ../22_for_plotting/.
+mv par*.a        ../22_for_plotting/.
+mv colat-r-t.a   ../22_for_plotting/.
+
 echo "   "
 echo "     *****************************************"
-echo "     *      PROGRAM  highrespec3d running    *"
+echo "     *      PROGRAM  lowrespec3d running     *"
 echo "     *****************************************"
 
 ./lowrespec3d.exe
+./mergefiles.exe
 
-mv *.params ../11_results/.
-mv *.a      ../11_results/.
+cp low-res-in*.a    ../11_results/.
+mv in*.params       ../11_results/.
+
+mv low-res-in*.a    ../22_for_plotting/.
+mv colat-r-t.a      ../22_for_plotting/.
+mv 4plots.a         ../22_for_plotting/.
+
+rm -f *.a
 
 echo "   "
-echo "     **********************************************"
-echo "     *       Synthetic spectrum (in physical      *"
-echo "     *       and stellar parameters are in        *"
-echo "     *       parameters are in folder 11_results  *"
-echo "     **********************************************"
+echo "     *******************************************************"
+echo "     *                                                     *"  
+echo "     *     Synthetic spectrum in physical units and        *"
+echo "     *     relevant stellar parameters are in folder:      *"
+echo "     *                                                     *"
+echo "     *     11_results/                                     *"
+echo "     *                                                     *"
+echo "     *     Additional output files for plotting,           *"
+echo "     *     including the spectra, are in folder:           *"
+echo "     *                                                     *"
+echo "     *     22_for_plotting/                                *"
+echo "     *                                                     *"
+echo "     *******************************************************"
 echo "   "
 
 exit
